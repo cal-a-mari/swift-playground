69158d497498da84f1013e1f63d250a233faa480
swift-nation <m.ari.batilando@gmail.com>
2016-07-24 21:49:12 -0700

    Add LinkedList compare

---
+++ algorithms/linked-list/CompareLinkedListStrings.swift
@@ -0,0 +1,55 @@
+//
+//  CompareLinkedListStrings.swift
+//  Test
+//
+//  Created by Mari Batilando on 7/24/16.
+//  Copyright © 2016 Mari Batilando. All rights reserved.
+//
+
+import Foundation
+
+extension LinkedList {
+    
+    static func compare(listA: LinkedList<Character>, listB: LinkedList<Character>) -> Int {
+        var ptrA = listA.head
+        var ptrB = listB.head
+        
+        while (ptrA != nil) && (ptrB != nil) && (ptrA?.value == ptrB?.value) {
+            ptrA = ptrA!.next
+            ptrB = ptrB!.next
+        }
+        
+        if (ptrA == nil) && (ptrB == nil) {
+            return 0
+        }
+        
+        if ptrA?.value > ptrB?.value {
+            return 1
+        } else {
+            return -1
+        }
+    }
+    
+}
+
+func ==(lhs: LinkedList<Character>, rhs: LinkedList<Character>) -> Bool {
+    return LinkedList<Character>.compare(listA: lhs, listB: rhs) == 0
+}
+
+func <(lhs: LinkedList<Character>, rhs: LinkedList<Character>) -> Bool {
+    return LinkedList<Character>.compare(listA: lhs, listB: rhs) == -1
+}
+
+func <=(lhs: LinkedList<Character>, rhs: LinkedList<Character>) -> Bool {
+    let res = LinkedList<Character>.compare(listA: lhs, listB: rhs)
+    return res == 0 || res == -1
+}
+
+func >(lhs: LinkedList<Character>, rhs: LinkedList<Character>) -> Bool {
+    return LinkedList<Character>.compare(listA: lhs, listB: rhs) == 1
+}
+
+func >=(lhs: LinkedList<Character>, rhs: LinkedList<Character>) -> Bool {
+    let res = LinkedList<Character>.compare(listA: lhs, listB: rhs)
+    return res == 0 || res == 1
+}
