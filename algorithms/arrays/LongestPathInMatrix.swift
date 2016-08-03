cecc8af6f52744e45f1c2d562a6ac4c04737b11e
swift-nation <m.ari.batilando@gmail.com>
2016-08-02 21:51:38 -0700

    Refactor LongestPathInMatrix

--- algorithms/arrays/LongestPathInMatrix.swift
+++ algorithms/arrays/LongestPathInMatrix.swift
@@ -8,42 +8,36 @@
 
 import Foundation
 
-func longestPath(inMatrix matrix: [[Int]]) -> Int {
-    var values = [Int: [Int: Int]]()
+func longestIncrementingPath(inMatrix matrix: [[Int]]) -> Int {
+    var values = Array(repeatElement(Array(repeatElement(-1, count: matrix.count)), count: matrix.count))
     
-    for i in 0..<matrix.endIndex {
-        for j in 0..<matrix.endIndex {
-            let _ = longestPathOfCell(inMatrix: matrix, i: i, j: j, values: &values)
+    for i in 0..<matrix.count {
+        for j in 0..<matrix.count {
+            let _  = longestIncrementingPath(inMatrix: matrix, i: i, j: j, values: &values)
         }
     }
     
-    let pathTuples = values.flatMap { return $1 }
-    let pathVals = pathTuples.map { return $1 }
-    
-    return pathVals.max()!
+    return values.flatMap { $0 }.max()!
 }
 
-private func longestPathOfCell(inMatrix matrix: [[Int]], i: Int, j: Int, values: inout [Int: [Int: Int]]) -> Int {
-    if i < 0 || j < 0 || i >= matrix.count || j >= matrix.count {
-        return 0
+private func longestIncrementingPath(inMatrix matrix: [[Int]], i: Int, j: Int, values: inout [[Int]]) -> Int {
+    if values[i][j] != -1 {
+        return values[i][j]
     }
     
-    if let memoizedValue = values[i]?[j] {
-        return memoizedValue
+    let nextValue = matrix[i][j] + 1
+    
+    if (i > 0) && (matrix[i - 1][j] == nextValue) {
+        values[i][j] = 1 + longestIncrementingPath(inMatrix: matrix, i: i - 1, j: j, values: &values)
+    } else if (i < matrix.count - 1) && (matrix[i + 1][j] == nextValue) {
+        values[i][j] = 1 + longestIncrementingPath(inMatrix: matrix, i: i + 1, j: j, values: &values)
+    } else if (j > 0) && (matrix[i][j - 1] == nextValue) {
+        values[i][j] = 1 + longestIncrementingPath(inMatrix: matrix, i: i, j: j - 1, values: &values)
+    } else if (j < matrix.count - 1) && (matrix[i][j + 1] == nextValue) {
+        values[i][j] = 1 + longestIncrementingPath(inMatrix: matrix, i: i, j: j + 1, values: &values)
     } else {
-        let nextNumber = matrix[i][j] + 1
-        values[i] = [j: 1]
-        
-        if (i > 0) && (matrix[i - 1][j] == nextNumber) {
-            values[i]![j] = 1 + longestPathOfCell(inMatrix: matrix, i: i - 1, j: j, values: &values)
-        } else if (i < matrix.count - 1) && (matrix[i + 1][j] == nextNumber) {
-            values[i]![j] = 1 + longestPathOfCell(inMatrix: matrix, i: i + 1, j: j, values: &values)
-        } else if (j > 0) && (matrix[i][j - 1] == nextNumber) {
-            values[i]![j] = 1 + longestPathOfCell(inMatrix: matrix, i: i, j: j - 1, values: &values)
-        } else if (j < matrix.count - 1) && (matrix[i][j + 1] == nextNumber) {
-            values[i]![j] = 1 + longestPathOfCell(inMatrix: matrix, i: i, j: j + 1, values: &values)
-        }
-        
-        return values[i]![j]!
+        values[i][j] = 1
     }
+    
+    return values[i][j]
 }
