9c06f65e26633134916e8f15a195e5ca6c7e69b4
swift-nation <m.ari.batilando@gmail.com>
2016-08-04 21:03:13 -0700

    Refactor insert into sorted list

--- algorithms/linked-list/InsertIntoSortedList.swift
+++ algorithms/linked-list/InsertIntoSortedList.swift
@@ -12,39 +12,37 @@ extension LinkedList {
     
     /// Note: This is for a sorted LinkedList
     func sortedInsert(value: T) {
+        let nodeToInsert = LinkedListNode(value: value)
+        
         guard let head = self.head else {
-            // If this is the first node going to be inserted
-            self.head = LinkedListNode<T>(value: value)
-            return
-        }
-        
-        let nodeToInsert = LinkedListNode<T>(value: value)
-        
-        if value < head.value {
-            // If this is the new head of the linked list
-            nodeToInsert.next = head
+            // Edge Case: If this is the first node to be inserted
             self.head = nodeToInsert
             return
         }
         
-        var ptrA = head
-        var ptrB = ptrA.next
-        
-        while ptrB != nil {
-            if value < ptrB!.value {
-                // Normal case for inserting a value in the middle of the list
-                nodeToInsert.next = ptrB!.next
-                ptrB!.next = nil
-                ptrA.next = nodeToInsert
-                return
-            } else {
-                ptrA = ptrB!
-                ptrB = ptrB!.next
-            }
+        // Edge Case: If this is the new head
+        if value < head.value {
+            nodeToInsert.next = head
+            self.head = nodeToInsert
         }
         
-        // Edge case for the node being the new tail
-        ptrA.next = nodeToInsert
+        var current = head
+        var next = current.next
+        
+        while next != nil {
+            if next!.value < value {
+                // Normal case if the new node is to be inserted in the middle of the list
+                current.next = nodeToInsert
+                nodeToInsert.next = next!
+                return
+            }
+            
+            current = next!
+            next = next!.next
+        }
+        
+        // Edge case: If this is the new tail of the list
+        current.next = nodeToInsert
     }
     
 }
