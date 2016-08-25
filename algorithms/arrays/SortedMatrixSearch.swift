4b9ba4dca59d7441ebae6b74a7d95f0b61f61742
off-by-1 <m.ari.batilando@gmail.com>
2016-08-25 09:39:58 -0700

    Update SortedMatrixSearch.swift

--- algorithms/arrays/SortedMatrixSearch.swift
+++ algorithms/arrays/SortedMatrixSearch.swift
@@ -31,3 +31,32 @@ func search(element: Int, inSortedMatrix matrix: [[Int]]) -> Bool {
     
     return false
 }
+
+//O(logn)
+func binarySearch(element: Int, inSortedMatrix matrix: [[Int]]) -> Bool {
+	guard matrix.count > 0 && matrix.first!.count > 0 else {
+		return false
+	}
+	
+	let m = matrix.count
+	let n = matrix.first!.count
+	
+	var start = 0
+	var end = m * n - 1
+	
+	while start <= end {
+		let mid = (start + end) / 2
+		let i = mid / n
+		let j = mid % n
+		
+		if matrix[i][j] == element {
+			return true
+		} else if matrix[i][j] < element {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+	
+	return false
+}
