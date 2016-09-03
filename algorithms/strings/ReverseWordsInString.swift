6ca82a4bacc23fbd0bc23ed50003e1e44c8ba5c9
swift-nation <m.ari.batilando@gmail.com>
2016-09-02 18:57:19 -0700

    ReverseWordsInString.swift

--- algorithms/strings/ReverseWordsInString.swift
+++ algorithms/strings/ReverseWordsInString.swift
@@ -7,28 +7,54 @@
 //
 
 import Foundation
-
-extension String {
+/*
+ Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
+ 
+ The input string does not contain leading or trailing spaces and the words are always separated by a single space.
+ 
+ For example,
+ Given s = "the sky is blue",
+ return "blue is sky the".
+ */
+// O(n) runtime and O(1) space
+func reverseWords(inString string: String) -> String {
+    var wordBegin = 0
+    var string = string.characters.map { $0 }
     
-    /*
-     Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
-     
-     The input string does not contain leading or trailing spaces and the words are always separated by a single space.
-     
-     For example,
-     Given s = "the sky is blue",
-     return "blue is sky the".
-     */
-    // This is the best I could do it in Swift
-    // The optimized way is to reverse each word then reverse the entire String
-    // O(n) runtime and O(n) space
-    var reversedWordsNaive: String {
-        let wordArr = self.components(separatedBy: " ")
-        return wordArr.reversed().joined(separator: " ")
+    for i in 0..<string.count {
+        if string[i] == Character(" ") {
+            reverse(array: &string, start: wordBegin, end: i - 1)
+            wordBegin = i + 1
+        }
     }
     
+    reverse(array: &string, start: wordBegin, end: string.count - 1) // reverse the last word
+    reverse(array: &string, start: 0, end: string.count - 1) // reverse the entire array
+    
+    return String(string)
 }
 
+private func reverse(array: inout [Character], start: Int, end: Int) {
+    var start = start
+    var end = end
+    
+    while start < end {
+        swap(&array[start], &array[end])
+        start += 1
+        end -= 1
+    }
+}
+
+func reverseWordsTests() {
+    let words1 = "the sky is blue"
+    print("Should be: blue is sky the: \(reverseWords(inString: words1))")
+    
+    let words2 = "i like dogs"
+    print("Should be: dogs like i: \(reverseWords(inString: words2))")
+    
+    let words3 = "i"
+    print("Should be i: \(reverseWords(inString: words3))")
+}
 
 
 
