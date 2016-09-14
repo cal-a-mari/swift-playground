d94facf6706ecceb58f0d18237563662272928cd
swift-nation <m.ari.batilando@gmail.com>
2016-09-13 20:46:49 -0700

    Refactor old problems

--- algorithms/arrays/FindFrequencyOfElement.swift
+++ algorithms/arrays/FindFrequencyOfElement.swift
@@ -52,3 +52,68 @@ extension Array where Element: Comparable {
     }
     
 }
+
+func findFreq(array: [Int], target: Int) -> Int {
+    if array.isEmpty {
+        return 0
+    }
+    
+    return findFreq(array: array, target: target, start: 0, end: array.count - 1)
+}
+
+private func findFreq(array: [Int], target: Int, start: Int, end: Int) -> Int {
+    if start <= end {
+        if start == end {
+            if array[start] == target {
+                return end - start + 1
+            }
+            
+            return 0
+        }
+        
+        let mid = (start + end) / 2
+        
+        if array[mid] <= target {
+            let left = findFreq(array: array, target: target, start: start, end: mid)
+            let right = findFreq(array: array, target: target, start: mid + 1, end: end)
+            return left + right
+        }
+        
+        if array[mid] > target {
+            return findFreq(array: array, target: target, start: start, end: mid)
+        }
+        
+        if array[mid] < target {
+            return findFreq(array: array, target: target, start: mid + 1, end: end)
+        }
+    }
+    
+    return 0
+}
+
+func findFreqTests() {
+    let arr1 = [1, 2, 2, 2, 3]
+    let res1 = 3
+    let actualRes1 = findFreq(array: arr1, target: 2)
+    assert(res1 == actualRes1)
+    
+    let arr2 = [1, 2, 3]
+    let res2 = 1
+    let actualRes2 = findFreq(array: arr2, target: 2)
+    assert(res2 == actualRes2)
+    
+    let arr3 = [1, 3]
+    let res3 = 0
+    let actualRes3 = findFreq(array: arr3, target: 2)
+    assert(res3 == actualRes3)
+    
+    let arr4 = [Int]()
+    let res4 = 0
+    let actualRes4 = findFreq(array: arr4, target: 2)
+    assert(res4 == actualRes4)
+    
+    let arr5 = [1]
+    let res5 = 0
+    let actualRes5 = findFreq(array: arr5, target: 2)
+    assert(res5 == actualRes5)
+}
--- algorithms/strings/Isomorphic.swift
+++ algorithms/strings/Isomorphic.swift
@@ -8,45 +8,54 @@
 
 import Foundation
 
-extension String {
-	
-	/*
-		Given two strings s and t, determine if they are isomorphic.
-		Two strings are isomorphic if the characters in s can be replaced to get t.
-	
-		All occurrences of a character must be replaced with another character while 
-		preserving the order of characters. No two characters may map to the
-		same character but a character may map to itself.
-	
-		For example,"egg" and "add" are isomorphic, "foo" and "bar" are not.
-	*/
-	// O(n)
-	func isIsomorphic(withString string: String) -> Bool {
-		guard self.characters.count == string.characters.count else {
-			return false
-		}
-		
-		var map = [Character: Character]()
+/*
+ Given two strings s and t, determine if they are isomorphic.
+ Two strings are isomorphic if the characters in s can be replaced to get t.
+ 
+ All occurrences of a character must be replaced with another character while
+ preserving the order of characters. No two characters may map to the
+ same character but a character may map to itself.
+ 
+ For example,"egg" and "add" are isomorphic, "foo" and "bar" are not.
+ */
+// O(n)
+func isIsomorphic(strA: String, strB: String) -> Bool {
+    let strA = strA.characters.map { $0 }
+    let strB = strB.characters.map { $0 }
+    
+    var map = [Character: Character]()
+    
+    if strA.count != strB.count {
+        return false
+    }
+    
+    for i in 0..<strB.count {
+        if let mappedChar = map[strA[i]] {
+            if strB[i] != mappedChar {
+                return false
+            }
+        } else {
+            if map.values.contains(strB[i]) {
+                return false
+            }
+            
+            map[strA[i]] = strB[i]
+        }
+    }
+    
+    return true
+}
 
-		for (i, char) in self.characters.enumerated() {
-			let otherChar = string.characters[string.index(string.startIndex, offsetBy: i)]
-			
-			if let mappedChar = map[char] {
-				if mappedChar != otherChar {
-					return false
-				}
-			} else {
-				if map.values.contains(otherChar) {
-					return false
-				}
-				
-				map[char] = otherChar
-			}
-		}
-		
-		return true
-		
-	}
-	
-	
+func isomorphicTests() {
+    let str1 = "egg"
+    let str2 = "add"
+    assert(isIsomorphic(strA: str1, strB: str2))
+    
+    let str3 = "aab"
+    let str4 = "xyz"
+    assert(!isIsomorphic(strA: str3, strB: str4))
+    
+    let str5 = "paper"
+    let str6 = "title"
+    assert(isIsomorphic(strA: str5, strB: str6))
 }
